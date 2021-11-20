fe7717a3e072b1f8b9627a8b03814c9c40cc9e43
Luis Espinosa <82414418+LuisJose28@users.noreply.github.com>
2021-11-20 00:39:16 -0500

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(){
+    int x, y;
+    int i = 0;
+    float calificaciones[4][3];
+
+    printf("\n* Calificaciones de los estudiantes *\n\n");
+
+    for ( x = 0; x < 4; x++)
+    {
+        printf("-Escriba su calificacion N°%i: ",x);
+        scanf("%f", &calificaciones[4][3]);
+
+    }
+    
+
+
+    return 0;
+}
